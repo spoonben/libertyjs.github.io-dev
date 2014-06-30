fc296c0fe496146420b79d0c5d27628985708d44
Jonathan Belcher <belcherj@gmail.com>
2014-06-30 18:28:25 -0400

    Added logos and additional pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,12 +8,24 @@ angular.module('libertyjsdotcomApp', [
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
+        templateUrl: 'partials/home',
+        controller: 'HomeCtrl'
+      })
+      .when('/schedule', {
+        templateUrl: 'partials/schedule',
+        controller: 'ScheduleCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'partials/about',
+        controller: 'AboutCtrl'
+      })
+      .when('/tickets', {
+        templateUrl: 'partials/tickets',
+        controller: 'TicketsCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
-      
+
     $locationProvider.html5Mode(true);
   });
--- app/scripts/controllers/tickets.js
+++ app/scripts/controllers/tickets.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('libertyjsdotcomApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('TicketsCtrl', function ($scope, $http) {
     // $http.get('/api/awesomeThings').success(function(awesomeThings) {
     //   $scope.awesomeThings = awesomeThings;
     // });
